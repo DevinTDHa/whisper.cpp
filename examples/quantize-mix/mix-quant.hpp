7eff30133809805d51253c15aef25ddc46c79ab8
Devin Ha <devin@trungducha.de>
2025-08-16 11:36:03 +0200

    init quantize-mix

---
+++ examples/quantize-mix/mix-quant.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ggml.h"
+
+#include <fstream>
+#include <vector>
+#include <string>
+
+
+bool ggml_common_quantize_0(
+        std::ifstream & finp,
+        std::ofstream & fout,
+        const ggml_ftype ftype,
+        const std::vector<std::string> & to_quant,
+        const std::vector<std::string> & to_skip);
